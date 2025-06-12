b305ab258ecf556f3362c3836aacc2346f85bb49
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-06-12 21:59:06 +0530

    created agent struct and expanded main

--- main.go
+++ main.go
@@ -11,4 +11,27 @@ import {
 
 func main() {
 	fmt.Println("Initializing agent...")
+	client := anthropic.NewClient()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	getUserMessage := func() (string, bool) {
+		if !scanner.Scan() {
+			return "", false
+		}
+		return scanner.Text(), true
+	}
+
+	agent := NewAgent(&client, getUserMessage)
+}
+
+func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+	return &Agent{
+		client:         client,
+		getUserMessage: getUserMessage,
+	}
+}
+
+type Agent struct {
+	client         *anthropic.Client
+	getUserMessage func() (string, bool)
 }

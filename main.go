0c73ca3c71a668d80e5dd197fbbdfa9cea979420
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-06-18 22:28:46 +0530

    Eliminated agent logic from main.go

--- main.go
+++ main.go
@@ -7,27 +7,7 @@ import {
 	"os"
 
 	"github.com/anthropic/anthropic-sdk-go"
-}
-
-type ToolDefinition struct {
-	Name        string                         `json:"name"`
-	Description string                         `json:"description"`
-	InputSchema anthropic.ToolInputSchemaParam `json:"input_schema"`
-	Function    func(input json.RawMessage) (string, error)
-}
-
-type Agent struct {
-	client         *anthropic.Client
-	getUserMessage func() (string, bool)
-	tools          []ToolDefinition
-}
-
-func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool), tools []ToolDefinition,) *Agent {
-	return &Agent{
-		client:         client,
-		getUserMessage: getUserMessage,
-		tools:          tools,
-	}
+	"agent"
 }
 
 func main() {
